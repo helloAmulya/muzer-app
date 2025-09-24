eac66381745f65272e3c8f19696d4aa13a98dfc2
helloAmulya <internatomix@gmail.com>
2025-09-24 18:51:58 +0530

    first commit

---
+++ app/api/components/Appbar.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+import React from 'react'
+
+export const Appbar = () => {
+const session = useSession()
+    return (
+        <div className='flex justify-between'>
+            <div>Muzer</div>
+            <div>
+                {session.data?.user && <button className='m-2 p-2 bg-blue-400'  onClick={() => signOut()}> Log Out</button>}
+                {!session.data?.user && <button className='m-2 p-2 bg-blue-400'  onClick={() => signIn()}> Sign In</button>}
+            </div>
+        </div>
+    )
+}
