22de6e2fd462f43f903b1da7823dc6923643e1ba
helloAmulya <internatomix@gmail.com>
2025-09-27 07:29:47 +0530

    feat: user data route added

---
+++ app/api/streams/my/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/app/lib/db"
+import { getServerSession } from "next-auth";
+
+
+export async function GET(req: NextRequest) {
+    const session = await getServerSession()
+    const user = await prisma.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    })
+
+    if (!user) {
+        return NextResponse.json({
+            message: "Unauthenticated"
+        }, {
+            status: 403
+        })
+    }
+
+    const streams = await prisma.stream.findMany({
+        where: {
+            userId: user.id ?? ""
+        }
+    })
+
+    return NextResponse.json({ streams })
+
+
+}
